a4f827f1b6ea05be41ad627d0c968d0beb72e4d5
sabhi128 <sabhiahm12@gmail.com>
2025-08-28 16:14:59 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div
+      className="h-screen w-screen bg-cover bg-center"
+      style={{
+        backgroundImage: "url('https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?auto=format&fit=crop&w=1350&q=80')",
+      }}
+    >
+      {/* Your Trello Clone Content */}
+      {/* <h1 className="text-white text-4xl font-bold p-6">Trello Clone</h1> */}
+    </div>
+  );
+};
+
+export default App;
